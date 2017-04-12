01a1c5ce3d6c9439149ac55312cdf95a460359be
Miyachi Katsuya <miyachi_katsuya@r.recruit.co.jp>
2017-04-12 16:19:34 +0900

    fix: remove 0, 1 from prime numbers

--- solutions/arrays-more/index.js
+++ solutions/arrays-more/index.js
@@ -1,7 +1,7 @@
 const numbers = [...Array(100).keys()];
 
 const primes = numbers.filter(
-  (num, i, nums) => num <= 3 || !nums.some((n) => n >= 2 && n < num && num % n === 0)
+  (num, i, nums) => num >= 2 && !nums.some((n) => n >= 2 && n < num && num % n === 0)
 ).map((num) => Number(num).toString(16))
 
 console.log(primes)
