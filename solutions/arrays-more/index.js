b53ab778429e5b61e3a702b35dade85b588d7a53
progfay <mano_shunsuke@r.recruit.co.jp>
2023-04-03 18:06:09 +0900

    `standard --fix`

--- solutions/arrays-more/index.js
+++ solutions/arrays-more/index.js
@@ -1,4 +1,4 @@
-const numbers = [...Array(100).keys()];
+const numbers = [...Array(100).keys()]
 
 const primes = numbers.filter(
   (num, i, nums) => num >= 2 && !nums.some((n) => n >= 2 && n < num && num % n === 0)
