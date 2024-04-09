cd7dc3dace58380a3d8b7b6822913eaf08702296
progfay <mano_shunsuke@r.recruit.co.jp>
2024-04-09 17:06:27 +0900

    fix problem: fizzbuzz

--- solutions/fizzbuzz/index.js
+++ solutions/fizzbuzz/index.js
@@ -1,2 +1,11 @@
-for (i = 0; i < 100; )
-  console.log((++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i); // eslint-disable-line
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
