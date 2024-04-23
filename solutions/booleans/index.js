c835d29466d08466e629d62ca9776d8b0e7f9ccf
progfay <mano_shunsuke@r.recruit.co.jp>
2024-04-23 10:40:47 +0900

    fix

--- solutions/booleans/index.js
+++ solutions/booleans/index.js
@@ -1,5 +1,5 @@
 console.log((0 === 1) === false);
-console.log((0 === 1) === true);
+console.log((0 === 1) === false);
 
 console.log((10 == "10") === true);
 console.log((10 === "10") === false);
@@ -9,7 +9,7 @@ console.log((1 && "") === "");
 console.log((0 && 10) === 0);
 console.log((1 && 10) === 10);
 
-console.log((0 || "") === 0);
-console.log((1 || "") === "");
+console.log((0 || "") === "");
+console.log((1 || "") === 1);
 console.log((0 || 10) === 10);
-console.log((1 || 10) === 10);
+console.log((1 || 10) === 1);
